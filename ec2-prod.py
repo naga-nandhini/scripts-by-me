8f8dac30d4316fdd38f484a3338f7d897910f095
naga-nandhini <133082568+naga-nandhini@users.noreply.github.com>
2023-05-10 12:15:58 +0530

    Add files via upload

---
+++ ec2-prod.py
@@ -0,0 +1,11 @@
+import boto3
+
+aws_con = boto3.session.Session(profile_name="prod")
+
+ec2_dashboard = aws_con.resource(service_name ='ec2')
+
+with open('instance-prod.txt' , 'w') as file:
+  for instance in ec2_dashboard.instances.all():
+    file.write(str(instance))
+    file.write(str(instance.state))
+    file.write('\n')
