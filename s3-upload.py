da166b7be674d041c16e3b8913e21fcea3c38462
naga-nandhini <133082568+naga-nandhini@users.noreply.github.com>
2023-05-11 14:56:34 +0530

    Delete s3-upload.py

--- s3-upload.py
+++
@@ -1,7 +0,0 @@
-import boto3 
-aws_console=boto3.session.Session(profile_name="nandy")
-client=aws_console.client("s3")
-bucket_name=input("Enter the bucket name: ")
-file_name=input("Enter the file name:")
-response = s3.client.upload_file(bucket_name=bucket_name,file_name=file_name)
-print("Uploaded Sucessfuly")
